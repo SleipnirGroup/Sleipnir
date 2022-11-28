253d7583a54290cd0510d0a865e4abb4e6bf0951
Tyler Veness <calcmogul@gmail.com>
2022-11-28 08:42:30 -0800

    Fix matrix block-to-block assignment

--- src/optimization/VariableMatrix.cpp
+++ src/optimization/VariableMatrix.cpp
@@ -120,7 +120,7 @@ SLEIPNIR_DLLEXPORT VariableMatrix operator*(const VariableMatrix& lhs,
 
   for (int i = 0; i < lhs.Rows(); ++i) {
     for (int j = 0; j < rhs.Cols(); ++j) {
-      autodiff::Variable sum = 0.0;
+      autodiff::Variable sum;
       for (int k = 0; k < lhs.Cols(); ++k) {
         sum += lhs.Autodiff(i, k) * rhs.Autodiff(k, j);
       }
@@ -164,7 +164,7 @@ VariableMatrix& VariableMatrix::operator*=(const VariableMatrix& rhs) {
 
   for (int i = 0; i < Rows(); ++i) {
     for (int j = 0; j < rhs.Cols(); ++j) {
-      autodiff::Variable sum = 0.0;
+      autodiff::Variable sum;
       for (int k = 0; k < Cols(); ++k) {
         sum += Autodiff(i, k) * rhs.Autodiff(k, j);
       }
