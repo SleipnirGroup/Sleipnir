b38403f8ef77b55d661c50854a269aff729b0124
Julia Schatz <2347689+juliaschatz@users.noreply.github.com>
2023-04-27 22:33:49 -0700

    Add OCPSolver interface (#51)
    Co-authored-by: Tyler Veness <calcmogul@gmail.com>

---
+++ include/sleipnir/control/OCPSolver.hpp
@@ -0,0 +1,257 @@
+// Copyright (c) Sleipnir contributors
+
+#pragma once
+
+#include <chrono>
+
+#include <Eigen/Core>
+
+#include "sleipnir/SymbolExports.hpp"
+#include "sleipnir/autodiff/VariableMatrix.hpp"
+#include "sleipnir/optimization/OptimizationProblem.hpp"
+
+namespace sleipnir {
+
+/**
+ * Function representing an explicit or implicit ODE, or a discrete state
+ * transition function.
+ *
+ * - Explicit: dx/dt = f(t, x, u, *)
+ * - Implicit: f(t, [x dx/dt]', u, *) = 0
+ * - State transition: xₖ₊₁ = f(t, xₖ, uₖ, dt)
+ */
+using DynamicsFunction =
+    std::function<VariableMatrix(const Variable&, const VariableMatrix&,
+                                 const VariableMatrix&, const Variable&)>;
+
+/**
+ * Constrain a fixed step OCP. This function is called numSteps + 1 times, once
+ * for each step. The arguments are time, X_i (numStates)x1, U_i (numInputs)x1,
+ * and the timestep of this segment.
+ */
+using OCPConstraintCallback =
+    std::function<void(const Variable&, const VariableMatrix&,
+                       const VariableMatrix&, const Variable&)>;
+
+/**
+ * Performs 4th order Runge-Kutta integration of dx/dt = f(t, x, u) for dt.
+ *
+ * @param f  The function to integrate. It must take two arguments x and u.
+ * @param x  The initial value of x.
+ * @param u  The value u held constant over the integration period.
+ * @param t0 The initial time.
+ * @param dt The time over which to integrate.
+ */
+template <typename F, typename State, typename Input, typename Time>
+State RK4(F&& f, State x, Input u, Time t0, Time dt) {
+  auto halfdt = dt * 0.5;
+  State k1 = f(t0, x, u, dt);
+  State k2 = f(t0 + halfdt, x + k1 * halfdt, u, dt);
+  State k3 = f(t0 + halfdt, x + k2 * halfdt, u, dt);
+  State k4 = f(t0 + dt, x + k3 * dt, u, dt);
+
+  return x + (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (dt / 6.0);
+}
+
+/**
+ * Enum describing an OCP transcription method.
+ */
+enum class TranscriptionMethod {
+  kDirectTranscription,
+  kDirectCollocation,
+  kSingleShooting
+};
+
+/**
+ * Enum describing a type of system dynamics constraints.
+ */
+enum class DynamicsType { kExplicitODE, kDiscrete };
+
+/**
+ * Enum describing the type of system timestep.
+ */
+enum class TimestepMethod { kFixed, kVariable, kVariableSingle };
+
+/**
+ * This class allows the user to pose and solve a constrained optimal control
+ * problem (OCP) in a variety of ways.
+ *
+ * The system is transcripted by one of three methods (direct collocation,
+ * direct transcription, or single-shooting) and additional constraints can be
+ * added.
+ *
+ * In single-shooting, states depend explicitly as a function of all previous
+ * states and all previous inputs. In direct transcription, each state is a
+ * decision variable constrained to the integrated dynamics of the previous
+ * state. In direct collocation, the trajectory is modeled as a series of cubic
+ * polynomials where the centerpoint slope is constrained.
+ *
+ * Explicit ODEs are integrated using RK4.
+ *
+ * For explicit ODEs, the function must be in the form dx/dt = f(t, x, u).
+ * For discrete state transition functions, the function must be in the form
+ * xₖ₊₁ = f(t, xₖ, uₖ).
+ *
+ * Direct collocation requires an explicit ODE. Direct transcription and
+ * single-shooting can use either an ODE or state transition function.
+ *
+ * https://underactuated.mit.edu/trajopt.html goes into more detail on each
+ * transcription method.
+ */
+class SLEIPNIR_DLLEXPORT OCPSolver : public OptimizationProblem {
+ public:
+  /**
+   * Build an optimization problem using a system evolution function (explicit
+   * ODE or discrete state transition function).
+   *
+   * @param numStates The number of system states.
+   * @param numInputs The number of system inputs.
+   * @param dt The timestep for fixed-step integration.
+   * @param numSteps The number of control points.
+   * @param dynamics The system evolution function, either an explicit ODE or a
+   * discrete state transition function.
+   * @param dynamicsType The type of system evolution function.
+   * @param timestepMethod The timestep method.
+   * @param method The transcription method.
+   */
+  OCPSolver(
+      int numStates, int numInputs, std::chrono::duration<double> dt,
+      int numSteps, const DynamicsFunction& dynamics,
+      DynamicsType dynamicsType = DynamicsType::kExplicitODE,
+      TimestepMethod timestepMethod = TimestepMethod::kFixed,
+      TranscriptionMethod method = TranscriptionMethod::kDirectTranscription);
+
+  /**
+   * Utility function to constrain the initial state.
+   *
+   * @param initialState the initial state to constrain to.
+   */
+  void ConstrainInitialState(const VariableMatrix& initialState);
+
+  /**
+   * Utility function to constrain the final state.
+   *
+   * @param finalState the final state to constrain to.
+   */
+
+  void ConstrainFinalState(const VariableMatrix& finalState);
+
+  /**
+   * Set the constraint evaluation function. This function is called
+   * `numSteps+1` times, with the corresponding state and input
+   * VariableMatrices.
+   *
+   * @param constraintFunction the constraint function.
+   */
+  void ConstrainAlways(const OCPConstraintCallback& constraintFunction);
+
+  /**
+   * Convenience function to set a lower bound on the input.
+   *
+   * @param lowerBound The lower bound that inputs must always be above. Must be
+   * shaped (numInputs)x1.
+   */
+  void SetLowerInputBound(const VariableMatrix& lowerBound) {
+    for (int i = 0; i < m_numSteps + 1; ++i) {
+      SubjectTo(U().Col(i) >= lowerBound);
+    }
+  }
+
+  /**
+   * Convenience function to set an upper bound on the input.
+   *
+   * @param upperBound The upper bound that inputs must always be below. Must be
+   * shaped (numInputs)x1.
+   */
+  void SetUpperInputBound(const VariableMatrix& upperBound) {
+    for (int i = 0; i < m_numSteps + 1; ++i) {
+      SubjectTo(U().Col(i) <= upperBound);
+    }
+  }
+
+  /**
+   * Convenience function to set an upper bound on the timestep.
+   *
+   * @param maxTimestep The maximum timestep.
+   */
+  void SetMaxTimestep(std::chrono::duration<double> maxTimestep) {
+    SubjectTo(DT() <= maxTimestep.count());
+  }
+
+  /**
+   * Convenience function to set a lower bound on the timestep.
+   *
+   * @param minTimestep The minimum timestep.
+   */
+  void SetMinTimestep(std::chrono::duration<double> minTimestep) {
+    SubjectTo(DT() >= minTimestep.count());
+  }
+
+  /**
+   * Get the state variables. After the problem is solved, this will contain the
+   * optimized trajectory.
+   *
+   * Shaped (numStates)x(numSteps+1).
+   *
+   * @returns The state variable matrix.
+   */
+  VariableMatrix& X() { return m_X; };
+
+  /**
+   * Get the input variables. After the problem is solved, this will contain the
+   * inputs corresponding to the optimized trajectory.
+   *
+   * Shaped (numInputs)x(numSteps+1), although the last input step is unused in
+   * the trajectory.
+   *
+   * @returns The input variable matrix.
+   */
+  VariableMatrix& U() { return m_U; };
+
+  /**
+   * Get the timestep variables. After the problem is solved, this will contain
+   * the timesteps corresponding to the optimized trajectory.
+   *
+   * Shaped 1x(numSteps+1), although the last timestep is unused in
+   * the trajectory.
+   *
+   * @returns The timestep variable matrix.
+   */
+  VariableMatrix& DT() { return m_DT; };
+
+  /**
+   * Convenience function to get the initial state in the trajectory.
+   *
+   * @returns The initial state of the trajectory.
+   */
+  VariableMatrix InitialState() { return m_X.Col(0); }
+
+  /**
+   * Convenience function to get the final state in the trajectory.
+   *
+   * @returns The final state of the trajectory.
+   */
+  VariableMatrix FinalState() { return m_X.Col(m_numSteps); }
+
+ private:
+  void ConstrainDirectCollocation();
+  void ConstrainDirectTranscription();
+  void ConstrainSingleShooting();
+
+  int m_numStates;
+  int m_numInputs;
+  std::chrono::duration<double> m_dt;
+  int m_numSteps;
+  TranscriptionMethod m_transcriptionMethod;
+
+  DynamicsType m_dynamicsType;
+  const DynamicsFunction& m_dynamicsFunction;
+
+  TimestepMethod m_timestepMethod;
+
+  VariableMatrix m_X;
+  VariableMatrix m_U;
+  VariableMatrix m_DT;
+};
+
+}  // namespace sleipnir
---
+++ test/src/control/OCPFlywheelTest.cpp
@@ -0,0 +1,178 @@
+// Copyright (c) Sleipnir contributors
+
+#include <chrono>
+#include <cmath>
+#include <fstream>
+
+#include <Eigen/Core>
+#include <Eigen/QR>
+#include <fmt/core.h>
+#include <gtest/gtest.h>
+#include <sleipnir/control/OCPSolver.hpp>
+#include <sleipnir/optimization/OptimizationProblem.hpp>
+#include <units/angle.h>
+#include <units/angular_acceleration.h>
+#include <units/angular_velocity.h>
+#include <units/time.h>
+#include <units/voltage.h>
+
+namespace {
+bool Near(double expected, double actual, double tolerance) {
+  return std::abs(expected - actual) < tolerance;
+}
+}  // namespace
+
+void TestFlywheel(std::string test_name, Eigen::Matrix<double, 1, 1> A,
+                  Eigen::Matrix<double, 1, 1> B,
+                  const sleipnir::DynamicsFunction& F,
+                  sleipnir::DynamicsType dynamicsType,
+                  sleipnir::TranscriptionMethod method) {
+  auto start = std::chrono::system_clock::now();
+
+  constexpr auto T = 5_s;
+  constexpr units::second_t dt = 5_ms;
+  constexpr int N = T / dt;
+
+  // Flywheel model:
+  // States: [velocity]
+  // Inputs: [voltage]
+  Eigen::Matrix<double, 1, 1> A_discrete{std::exp(A(0) * dt.value())};
+  Eigen::Matrix<double, 1, 1> B_discrete{(1.0 - A_discrete(0)) * B(0)};
+  Eigen::Matrix<double, 1, 1> r{10.0};
+
+  sleipnir::OCPSolver solver(1, 1, std::chrono::duration<double>(dt.value()), N,
+                             F, dynamicsType, sleipnir::TimestepMethod::kFixed,
+                             method);
+  solver.ConstrainInitialState(0.0);
+  solver.SetUpperInputBound(12);
+  solver.SetLowerInputBound(-12);
+  // Set up objective
+  Eigen::Matrix<double, 1, N + 1> r_mat =
+      r * Eigen::Matrix<double, 1, N + 1>::Ones();
+  sleipnir::VariableMatrix r_mat_vmat{r_mat};
+  sleipnir::VariableMatrix objective =
+      (r_mat_vmat - solver.X()) * (r_mat_vmat - solver.X()).T();
+  solver.Minimize(objective);
+
+  auto end1 = std::chrono::system_clock::now();
+  using std::chrono::duration_cast;
+  using std::chrono::microseconds;
+  fmt::print("Setup time: {} ms\n\n",
+             duration_cast<microseconds>(end1 - start).count() / 1000.0);
+
+  auto status = solver.Solve({.diagnostics = true});
+
+  EXPECT_EQ(sleipnir::ExpressionType::kQuadratic, status.costFunctionType);
+  EXPECT_EQ(sleipnir::ExpressionType::kLinear, status.equalityConstraintType);
+  EXPECT_EQ(sleipnir::ExpressionType::kLinear, status.inequalityConstraintType);
+  EXPECT_EQ(sleipnir::SolverExitCondition::kOk, status.exitCondition);
+
+  // Voltage for steady-state velocity:
+  //
+  // rₖ₊₁ = Arₖ + Buₖ
+  // uₖ = B⁺(rₖ₊₁ − Arₖ)
+  // uₖ = B⁺(rₖ − Arₖ)
+  // uₖ = B⁺(I − A)rₖ
+  Eigen::Matrix<double, 1, 1> u_ss =
+      B_discrete.householderQr().solve(decltype(A_discrete)::Identity() -
+                                       A_discrete) *
+      r;
+
+  // Verify solution
+  Eigen::Matrix<double, 1, 1> x{0.0};
+  Eigen::Matrix<double, 1, 1> u{0.0};
+  for (int k = 0; k < N; ++k) {
+    // Verify state
+    EXPECT_NEAR(x(0), solver.X().Value(0, k), 1e-2)
+        << fmt::format("  k = {}", k);
+
+    // Determine expected input for this timestep
+    double error = r(0) - x(0);
+    if (error > 1e-2) {
+      // Max control input until the reference is reached
+      u(0) = 12.0;
+    } else {
+      // Maintain speed
+      u = u_ss;
+    }
+
+    // Verify input
+    if (k > 0 && k < N - 1 && Near(12.0, solver.U().Value(0, k - 1), 1e-2) &&
+        Near(u_ss(0), solver.U().Value(0, k + 1), 1e-2)) {
+      // If control input is transitioning between 12 and u_ss, ensure it's
+      // within (u_ss, 12)
+      EXPECT_GE(u(0), u_ss(0)) << fmt::format("  k = {}", k);
+      EXPECT_LE(u(0), 12.0) << fmt::format("  k = {}", k);
+    } else {
+      EXPECT_NEAR(u(0), solver.U().Value(0, k), 1e-2)
+          << fmt::format("  k = {}", k);
+    }
+
+    // Project state forward
+    x = A_discrete * x + B_discrete * u;
+  }
+
+  // Verify final state
+  EXPECT_NEAR(r(0), solver.X().Value(0, N - 1), 1e-2);
+
+  // Log states for offline viewing
+  std::ofstream states{fmt::format("{} Flywheel states.csv", test_name)};
+  if (states.is_open()) {
+    states << "Time (s),Velocity (rad/s)\n";
+
+    for (int k = 0; k < N + 1; ++k) {
+      states << fmt::format("{},{}\n", k * units::second_t{dt}.value(),
+                            solver.X().Value(0, k));
+    }
+  }
+
+  // Log inputs for offline viewing
+  std::ofstream inputs{fmt::format("{} Flywheel inputs.csv", test_name)};
+  if (inputs.is_open()) {
+    inputs << "Time (s),Voltage (V)\n";
+
+    for (int k = 0; k < N + 1; ++k) {
+      if (k < N) {
+        inputs << fmt::format("{},{}\n", k * dt.value(),
+                              solver.U().Value(0, k));
+      } else {
+        inputs << fmt::format("{},{}\n", k * dt.value(), 0.0);
+      }
+    }
+  }
+}
+
+TEST(OCPSolverTest, FlywheelExplicit) {
+  Eigen::Matrix<double, 1, 1> A{-1.0};
+  Eigen::Matrix<double, 1, 1> B{1.0};
+  auto f_ode = [=](sleipnir::Variable t, sleipnir::VariableMatrix x,
+                   sleipnir::VariableMatrix u,
+                   sleipnir::Variable dt) { return A * x + B * u; };
+  TestFlywheel("Explicit Collocation", A, B, f_ode,
+               sleipnir::DynamicsType::kExplicitODE,
+               sleipnir::TranscriptionMethod::kDirectCollocation);
+  TestFlywheel("Explicit Transcription", A, B, f_ode,
+               sleipnir::DynamicsType::kExplicitODE,
+               sleipnir::TranscriptionMethod::kDirectTranscription);
+  TestFlywheel("Explicit Single-Shooting", A, B, f_ode,
+               sleipnir::DynamicsType::kExplicitODE,
+               sleipnir::TranscriptionMethod::kSingleShooting);
+}
+
+TEST(OCPSolverTest, FlywheelDiscrete) {
+  Eigen::Matrix<double, 1, 1> A{-1.0};
+  Eigen::Matrix<double, 1, 1> B{1.0};
+  units::second_t dt = 5_ms;
+  Eigen::Matrix<double, 1, 1> A_discrete{std::exp(A(0) * dt.value())};
+  Eigen::Matrix<double, 1, 1> B_discrete{(1.0 - A_discrete(0)) * B(0)};
+  auto f_discrete = [=](sleipnir::Variable t, sleipnir::VariableMatrix x,
+                        sleipnir::VariableMatrix u, sleipnir::Variable dt) {
+    return A_discrete * x + B_discrete * u;
+  };
+  TestFlywheel("Discrete Transcription", A, B, f_discrete,
+               sleipnir::DynamicsType::kDiscrete,
+               sleipnir::TranscriptionMethod::kDirectTranscription);
+  TestFlywheel("Discrete Single-Shooting", A, B, f_discrete,
+               sleipnir::DynamicsType::kDiscrete,
+               sleipnir::TranscriptionMethod::kSingleShooting);
+}
