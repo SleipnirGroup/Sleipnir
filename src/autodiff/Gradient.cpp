a80ad10d222ea38512ed9d9c7eb3ab9b78f35dcf
Tyler Veness <calcmogul@gmail.com>
2023-05-20 22:34:41 -0700

    Make Gradient constructor argument a value type

--- src/autodiff/Gradient.cpp
+++ src/autodiff/Gradient.cpp
@@ -7,7 +7,7 @@ using namespace sleipnir;
 Gradient::Gradient(Variable variable, Variable wrt) noexcept
     : Gradient{std::move(variable), MapVectorXvar{&wrt, 1}} {}
 
-Gradient::Gradient(Variable variable, Eigen::Ref<VectorXvar> wrt) noexcept
+Gradient::Gradient(Variable variable, VectorXvar wrt) noexcept
     : m_jacobian{MapVectorXvar(&variable, 1), std::move(wrt)} {}
 
 const Eigen::SparseVector<double>& Gradient::Calculate() {
