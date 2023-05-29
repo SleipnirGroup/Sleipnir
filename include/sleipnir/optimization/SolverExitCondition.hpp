86d3f70892bcfcc592cc9203a732edbf1e6f5a5e
Tyler Veness <calcmogul@gmail.com>
2023-05-29 15:21:22 -0700

    Give each numerical issue a unique exit condition (#136)

--- include/sleipnir/optimization/SolverExitCondition.hpp
+++ include/sleipnir/optimization/SolverExitCondition.hpp
@@ -14,8 +14,12 @@ enum class SolverExitCondition {
   kTooFewDOFs,
   /// The solver determined the problem to be locally infeasible and gave up.
   kLocallyInfeasible,
-  /// The solver failed to reach the desired tolerance due to a numerical issue.
-  kNumericalIssue,
+  /// The solver failed to reach the desired tolerance due to a numerical issue
+  /// (bad step).
+  kNumericalIssue_BadStep,
+  /// The solver failed to reach the desired tolerance due to a numerical issue
+  /// (max step too small).
+  kNumericalIssue_MaxStepTooSmall,
   /// The solver returned its solution so far after exceeding the maximum number
   /// of iterations.
   kMaxIterations,
