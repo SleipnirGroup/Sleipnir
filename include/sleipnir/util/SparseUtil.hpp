5bcc2faf2f3ebc5a7acfde2a7a59c4bfe46cfa0e
Tyler Veness <calcmogul@gmail.com>
2023-06-12 20:13:51 -0700

    Implement sparse lp-norm and add SparseUtil tests

--- include/sleipnir/util/SparseUtil.hpp
+++ include/sleipnir/util/SparseUtil.hpp
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include <algorithm>
+#include <concepts>
 #include <fstream>
 #include <vector>
 
@@ -28,6 +30,40 @@ SLEIPNIR_DLLEXPORT Eigen::SparseMatrix<double> SparseDiagonal(
  */
 SLEIPNIR_DLLEXPORT Eigen::SparseMatrix<double> SparseIdentity(int rows,
                                                               int cols);
+
+/**
+ * Returns lₚ-norm of a sparse matrix.
+ *
+ * @tparam Power Power of the norm (e.g., 2-norm). Eigen::Infinity refers to the
+ *   ∞-norm.
+ */
+template <int Power, typename Derived>
+  requires std::derived_from<Derived, Eigen::SparseCompressedBase<Derived>>
+double SparseLpNorm(const Derived& mat) {
+  double norm = 0.0;
+
+  for (int k = 0; k < mat.outerSize(); ++k) {
+    for (typename Derived::InnerIterator it{mat, k}; it; ++it) {
+      if constexpr (Power == 1) {
+        norm += std::abs(it.value());
+      } else if constexpr (Power == 2) {
+        norm += it.value() * it.value();
+      } else if constexpr (Power == Eigen::Infinity) {
+        norm = std::max(std::abs(norm), std::abs(it.value()));
+      } else {
+        norm += std::pow(std::abs(it.value()), Power);
+      }
+    }
+  }
+
+  if constexpr (Power == 1 || Power == Eigen::Infinity) {
+    return norm;
+  } else if constexpr (Power == 2) {
+    return std::sqrt(norm);
+  } else {
+    return std::pow(norm, 1.0 / Power);
+  }
+}
 
 /**
  * Adds a sparse matrix to the list of triplets with the given row and column
