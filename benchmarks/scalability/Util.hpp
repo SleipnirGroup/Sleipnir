054f5d88a95bb39728614cc9f46a112ce6ef9214
Tyler Veness <calcmogul@gmail.com>
2023-11-15 20:07:23 -0800

    Refactor scalability benchmark

--- benchmarks/scalability/Util.hpp
+++ benchmarks/scalability/Util.hpp
@@ -3,12 +3,15 @@
 #pragma once
 
 #include <chrono>
+#include <concepts>
 #include <fstream>
 #include <functional>
 #include <span>
+#include <string>
 #include <string_view>
 
 #include <casadi/casadi.hpp>
+#include <fmt/core.h>
 #include <sleipnir/optimization/OptimizationProblem.hpp>
 #include <units/time.h>
 
@@ -65,6 +68,8 @@ void RunBenchmark(std::ofstream& results, std::function<Problem()> setup,
  * The scale of the problem is iteratively increased by increasing the number of
  * timesteps within the time horizon.
  *
+ * @tparam Problem The optimization problem's type (casadi::Opti or
+ *   sleipnir::OptimizationProblem).
  * @param filename Results CSV filename.
  * @param diagnostics Whether to enable diagnostic prints.
  * @param T The time horizon of the optimization problem.
@@ -73,14 +78,49 @@ void RunBenchmark(std::ofstream& results, std::function<Problem()> setup,
  *   problem.
  * @param maxPower The maximum power of 10 for the number of samples in the
  *   problem.
- * @param casadiSetup A function that takes a time horizon and number of samples
- *   and returns a CasADi optimization problem instance.
  * @param sleipnirSetup A function that takes a time horizon and number of
- *   samples and returns a Sleipnir optimization problem instance.
+ *   samples and returns an optimization problem instance.
  */
-int RunBenchmarksAndLog(
-    std::string_view filename, bool diagnostics, units::second_t T,
-    std::span<int> sampleSizesToTest,
-    std::function<casadi::Opti(units::second_t, int)> casadiSetup,
-    std::function<sleipnir::OptimizationProblem(units::second_t, int)>
-        sleipnirSetup);
+template <typename Problem>
+int RunBenchmarksAndLog(std::string_view filename, bool diagnostics,
+                        units::second_t T, std::span<int> sampleSizesToTest,
+                        std::function<Problem(units::second_t, int)> setup) {
+  std::ofstream results{std::string{filename}};
+  if (!results.is_open()) {
+    return 1;
+  }
+
+  results << "Samples,"
+          << "Setup time (ms),Solve time (ms)\n";
+  std::flush(results);
+
+  for (int N : sampleSizesToTest) {
+    results << N << ",";
+    std::flush(results);
+
+    units::second_t dt = T / N;
+
+    fmt::print(stderr, "N = {}...", N);
+    RunBenchmark<Problem>(
+        results, [=] { return setup(dt, N); },
+        [=](Problem& problem) {
+          if constexpr (std::same_as<Problem, casadi::Opti>) {
+            if (diagnostics) {
+              problem.solver("ipopt");
+            } else {
+              problem.solver("ipopt", {{"print_time", 0}},
+                             {{"print_level", 0}, {"sb", "yes"}});
+            }
+            problem.solve();
+          } else {
+            problem.Solve({.diagnostics = diagnostics});
+          }
+        });
+    fmt::print(stderr, " done.\n");
+
+    results << "\n";
+    std::flush(results);
+  }
+
+  return 0;
+}
--- tools/generate-scalability-results-sleipnir.sh
+++ tools/generate-scalability-results-sleipnir.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-./build/FlywheelScalabilityBenchmark --casadi
 ./build/FlywheelScalabilityBenchmark --sleipnir
 ./tools/plot_scalability_results.py \
   --filenames \
-    flywheel-scalability-results-casadi.csv \
     flywheel-scalability-results-sleipnir.csv \
   --labels \
-    "CasADi + ipopt" \
     "Sleipnir" \
   --title Flywheel \
   --noninteractive
 
-./build/CartPoleScalabilityBenchmark --casadi
 ./build/CartPoleScalabilityBenchmark --sleipnir
 ./tools/plot_scalability_results.py \
   --filenames \
-    cart-pole-scalability-results-casadi.csv \
     cart-pole-scalability-results-sleipnir.csv \
   --labels \
-    "CasADi + ipopt" \
     "Sleipnir" \
   --title Cart-pole \
   --noninteractive
