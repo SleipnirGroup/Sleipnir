67c401464c16120489360eb64167eb2c0a114999
Tyler Veness <calcmogul@gmail.com>
2023-12-08 17:03:06 -0800

    Document enum classes and Zero() (#215)

--- include/sleipnir/autodiff/Expression.hpp
+++ include/sleipnir/autodiff/Expression.hpp
@@ -15,10 +15,30 @@
 
 namespace sleipnir {
 
-enum class ExpressionType { kNone, kConstant, kLinear, kQuadratic, kNonlinear };
+/**
+ * Expression type.
+ *
+ * Used for autodiff caching.
+ */
+enum class ExpressionType {
+  /// There is no expression.
+  kNone,
+  /// The expression is a constant.
+  kConstant,
+  /// The expression is composed of linear and lower-order operators.
+  kLinear,
+  /// The expression is composed of quadratic and lower-order operators.
+  kQuadratic,
+  /// The expression is composed of nonlinear and lower-order operators.
+  kNonlinear
+};
 
 struct SLEIPNIR_DLLEXPORT Expression;
 
+/**
+ * Returns an instance of "zero", which has special meaning in expression
+ * operations.
+ */
 SLEIPNIR_DLLEXPORT IntrusiveSharedPtr<Expression>& Zero();
 
 /**
