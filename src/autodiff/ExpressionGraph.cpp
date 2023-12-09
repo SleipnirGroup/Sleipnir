2d6d23ab81ee08321927bb4745a4e21b24eca314
Tyler Veness <calcmogul@gmail.com>
2023-12-08 16:04:48 -0800

    Rename MakeExpression() to MakeExpressionPtr()

--- src/autodiff/ExpressionGraph.cpp
+++ src/autodiff/ExpressionGraph.cpp
@@ -99,7 +99,7 @@ VectorXvar ExpressionGraph::GenerateGradientTree(Eigen::Ref<VectorXvar> wrt) {
   // Zero adjoints. The root node's adjoint is 1.0 as df/df is always 1.
   if (m_adjointList.size() > 0) {
     m_adjointList[0]->adjointExpr =
-        MakeExpression(1.0, ExpressionType::kConstant);
+        MakeExpressionPtr(1.0, ExpressionType::kConstant);
     for (auto it = m_adjointList.begin() + 1; it != m_adjointList.end(); ++it) {
       auto& node = *it;
       node->adjointExpr = Zero();
--- src/optimization/OptimizationProblem.cpp
+++ src/optimization/OptimizationProblem.cpp
@@ -94,7 +94,7 @@ OptimizationProblem::OptimizationProblem() noexcept {
 
 Variable OptimizationProblem::DecisionVariable() {
   m_decisionVariables.emplace_back(
-      MakeExpression(0.0, ExpressionType::kLinear));
+      MakeExpressionPtr(0.0, ExpressionType::kLinear));
   return m_decisionVariables.back();
 }
 
@@ -106,7 +106,7 @@ VariableMatrix OptimizationProblem::DecisionVariable(int rows, int cols) {
   for (int row = 0; row < rows; ++row) {
     for (int col = 0; col < cols; ++col) {
       m_decisionVariables.emplace_back(
-          MakeExpression(0.0, ExpressionType::kLinear));
+          MakeExpressionPtr(0.0, ExpressionType::kLinear));
       vars(row, col) = m_decisionVariables.back();
     }
   }
@@ -125,7 +125,7 @@ VariableMatrix OptimizationProblem::SymmetricDecisionVariable(int rows) {
   for (int row = 0; row < rows; ++row) {
     for (int col = 0; col <= row; ++col) {
       m_decisionVariables.emplace_back(
-          MakeExpression(0.0, ExpressionType::kLinear));
+          MakeExpressionPtr(0.0, ExpressionType::kLinear));
       vars(row, col) = m_decisionVariables.back();
       vars(col, row) = m_decisionVariables.back();
     }
