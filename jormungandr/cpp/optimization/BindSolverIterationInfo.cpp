207ab7ad90431758d78963cb93e4b69b0b82af12
Tyler Veness <calcmogul@gmail.com>
2023-12-11 12:12:58 -0800

    Add Python wrapper for OptimizationProblem::Callback()

---
+++ jormungandr/cpp/optimization/BindSolverIterationInfo.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) Sleipnir contributors
+
+#include <sleipnir/optimization/SolverIterationInfo.hpp>
+
+#include "optimization/BindSolverStatus.hpp"
+
+namespace py = pybind11;
+
+namespace sleipnir {
+
+void BindSolverIterationInfo(py::module_& optimization) {
+  py::class_<SolverIterationInfo> cls{optimization, "SolverIterationInfo"};
+  cls.def_readonly("iteration", &SolverIterationInfo::iteration);
+  cls.def_property_readonly(
+      "x", [](const SolverIterationInfo& self) { return self.x; });
+  cls.def_property_readonly(
+      "g", [](const SolverIterationInfo& self) { return self.g; });
+  cls.def_property_readonly(
+      "H", [](const SolverIterationInfo& self) { return self.H; });
+  cls.def_property_readonly(
+      "A_e", [](const SolverIterationInfo& self) { return self.A_e; });
+  cls.def_property_readonly(
+      "A_i", [](const SolverIterationInfo& self) { return self.A_i; });
+}
+
+}  // namespace sleipnir
