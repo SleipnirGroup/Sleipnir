eb3c431552eb97660aac3a1fc41255c68bf4b8ad
Tyler Veness <calcmogul@gmail.com>
2023-12-11 16:35:26 -0800

    Add more Python wrappers (#229)
    * Initializer list constructors
    * Zero() and Ones() static factory functions
    * shape attribute for matrix sizes
    * Block() free function

--- jormungandr/test/autodiff/variable_matrix_test.py
+++ jormungandr/test/autodiff/variable_matrix_test.py
@@ -9,11 +9,13 @@ def test_assignment_to_default():
 
     assert mat.rows() == 0
     assert mat.cols() == 0
+    assert mat.shape == (0, 0)
 
     mat = VariableMatrix(2, 2)
 
     assert mat.rows() == 2
     assert mat.cols() == 2
+    assert mat.shape == (2, 2)
     assert mat[0, 0] == 0.0
     assert mat[0, 1] == 0.0
     assert mat[1, 0] == 0.0
@@ -32,8 +34,7 @@ def test_assignment_to_default():
 
 def test_cwise_transform():
     # VariableMatrix CwiseTransform
-    A = VariableMatrix(2, 3)
-    A.set_value(np.array([[-2.0, -3.0, -4.0], [-5.0, -6.0, -7.0]]))
+    A = VariableMatrix([[-2.0, -3.0, -4.0], [-5.0, -6.0, -7.0]])
 
     result1 = A.cwise_transform(autodiff.abs)
     expected1 = np.array([[2.0, 3.0, 4.0], [5.0, 6.0, 7.0]])
@@ -56,11 +57,44 @@ def test_cwise_transform():
     assert (expected2 == result2.value()).all()
 
 
+def test_zero_static_function():
+    A = VariableMatrix.zero(2, 3)
+
+    for row in range(A.rows()):
+        for col in range(A.cols()):
+            assert A[row, col].value() == 0.0
+
+
+def test_ones_static_function():
+    A = VariableMatrix.ones(2, 3)
+
+    for row in range(A.rows()):
+        for col in range(A.cols()):
+            assert A[row, col].value() == 1.0
+
+
 def test_cwise_reduce():
-    A = VariableMatrix(2, 3)
-    A.set_value(np.array([[2.0, 3.0, 4.0], [5.0, 6.0, 7.0]]))
-    B = VariableMatrix(2, 3)
-    B.set_value(np.array([[8.0, 9.0, 10.0], [11.0, 12.0, 13.0]]))
+    A = VariableMatrix([[2.0, 3.0, 4.0], [5.0, 6.0, 7.0]])
+    B = VariableMatrix([[8.0, 9.0, 10.0], [11.0, 12.0, 13.0]])
     result = autodiff.cwise_reduce(A, B, lambda a, b: a * b)
 
     assert (np.array([[16.0, 27.0, 40.0], [55.0, 72.0, 91.0]]) == result.value()).all()
+
+
+def test_block_free_function():
+    A = VariableMatrix([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    B = VariableMatrix([[7.0], [8.0]])
+
+    mat1 = autodiff.block([[A, B]])
+    expected1 = np.array([[1.0, 2.0, 3.0, 7.0], [4.0, 5.0, 6.0, 8.0]])
+    assert mat1.shape == (2, 4)
+    assert (expected1 == mat1.value()).all()
+
+    C = VariableMatrix([[9.0, 10.0, 11.0, 12.0]])
+
+    mat2 = autodiff.block([[A, B], [C]])
+    expected2 = np.array(
+        [[1.0, 2.0, 3.0, 7.0], [4.0, 5.0, 6.0, 8.0], [9.0, 10.0, 11.0, 12.0]]
+    )
+    assert mat2.shape == (3, 4)
+    assert (expected2 == mat2.value()).all()
