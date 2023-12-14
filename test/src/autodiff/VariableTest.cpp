807050cbf67823f91f272b1db8434a6306057d67
Tyler Veness <calcmogul@gmail.com>
2023-12-13 21:06:24 -0800

    Add Expression constructor test

---
+++ test/src/autodiff/VariableTest.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) Sleipnir contributors
+
+#include <gtest/gtest.h>
+#include <sleipnir/autodiff/Variable.hpp>
+
+TEST(VariableTest, DefaultConstructor) {
+  sleipnir::Variable a;
+
+  EXPECT_EQ(0.0, a.Value());
+  EXPECT_EQ(sleipnir::ExpressionType::kConstant, a.expr->type);
+}
