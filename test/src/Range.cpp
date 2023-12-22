a1a89528d8d76cb6c4d374725a4d2ca0801cde0c
Tyler Veness <calcmogul@gmail.com>
2023-12-22 00:55:22 -0800

    Add Rosenbrock Hessian test

---
+++ test/src/Range.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) Sleipnir contributors
+
+#include "Range.hpp"
+
+std::vector<double> Range(double start, double end, double step) {
+  std::vector<double> ret;
+
+  int steps = (end - start) / step;
+  for (int i = 0; i < steps; ++i) {
+    ret.emplace_back(start + i * step);
+  }
+
+  return ret;
+}
