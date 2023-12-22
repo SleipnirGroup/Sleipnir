a55a3d5dd88e3f1098357385c140cd882b1bfb1e
Tyler Veness <calcmogul@gmail.com>
2023-12-21 22:33:55 -0800

    Implement feasible IPM

--- include/sleipnir/optimization/SolverConfig.hpp
+++ include/sleipnir/optimization/SolverConfig.hpp
@@ -23,6 +23,14 @@ struct SLEIPNIR_DLLEXPORT SolverConfig {
   std::chrono::duration<double> timeout{
       std::numeric_limits<double>::infinity()};
 
+  /// Enables the feasible interior-point method. When the inequality
+  /// constraints are all feasible, step sizes are reduced when necessary to
+  /// prevent them becoming infeasible again. This is useful when parts of the
+  /// problem are ill-conditioned in infeasible regions (e.g., square root of a
+  /// negative value). This can slow or prevent progress toward a solution
+  /// though, so only enable it if necessary.
+  bool feasibleIPM = false;
+
   /// Enables diagnostic prints.
   bool diagnostics = false;
 
