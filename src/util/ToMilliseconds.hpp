c15a7b9d9c86e0267688b4b76415a9d999ed4fbf
Tyler Veness <calcmogul@gmail.com>
2023-12-23 01:54:46 -0800

    Add more comments to blocks of interior-point method (#284)

---
+++ src/util/ToMilliseconds.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) Sleipnir contributors
+
+#pragma once
+
+#include <chrono>
+
+namespace sleipnir {
+
+/**
+ * Converts std::chrono::duration to a number of milliseconds rounded to three
+ * decimals.
+ */
+template <typename Rep, typename Period = std::ratio<1>>
+double ToMilliseconds(const std::chrono::duration<Rep, Period>& duration) {
+  using std::chrono::duration_cast;
+  using std::chrono::microseconds;
+  return duration_cast<microseconds>(duration).count() / 1000.0;
+}
+
+}  // namespace sleipnir
