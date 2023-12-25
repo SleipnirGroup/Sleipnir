6f5b49816e831496d6462d631b0b529c0ff0a6e0
Tyler Veness <calcmogul@gmail.com>
2023-12-25 14:00:21 -0800

    Move RK4 to separate test header

--- test/include/CartPoleUtil.hpp
+++ test/include/CartPoleUtil.hpp
@@ -4,27 +4,6 @@
 
 #include <Eigen/Core>
 #include <sleipnir/autodiff/VariableMatrix.hpp>
-#include <units/time.h>
-
-/**
- * Performs 4th order Runge-Kutta integration of dx/dt = f(x, u) for dt.
- *
- * @param f  The function to integrate. It must take two arguments x and u.
- * @param x  The initial value of x.
- * @param u  The value u held constant over the integration period.
- * @param dt The time over which to integrate.
- */
-template <typename F, typename T, typename U>
-T RK4(F&& f, T x, U u, units::second_t dt) {
-  const auto h = dt.value();
-
-  T k1 = f(x, u);
-  T k2 = f(x + h * 0.5 * k1, u);
-  T k3 = f(x + h * 0.5 * k2, u);
-  T k4 = f(x + h * k3, u);
-
-  return x + h / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
-}
 
 Eigen::Vector<double, 4> CartPoleDynamicsDouble(
     const Eigen::Vector<double, 4>& x, const Eigen::Vector<double, 1>& u);
