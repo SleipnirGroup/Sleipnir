ff928b67452a967de1ea2d06e860e493c14bdb82
Tyler Veness <calcmogul@gmail.com>
2024-01-14 22:31:14 -0800

    Make Python __len__() match numpy behavior (#339)

--- jormungandr/cpp/autodiff/BindVariableMatrices.cpp
+++ jormungandr/cpp/autodiff/BindVariableMatrices.cpp
@@ -512,7 +512,7 @@ void BindVariableMatrix(py::module_& autodiff,
         return lhs >= rhs;
       },
       py::is_operator());
-  variable_matrix.def("__len__", &VariableMatrix::size);
+  variable_matrix.def("__len__", &VariableMatrix::Rows);
 }
 
 void BindVariableBlock(
@@ -878,7 +878,7 @@ void BindVariableBlock(
   variable_block.def(py::self >= int());
   variable_block.def(double() >= py::self);
   variable_block.def(int() >= py::self);
-  variable_block.def("__len__", &VariableBlock<VariableMatrix>::size);
+  variable_block.def("__len__", &VariableBlock<VariableMatrix>::Rows);
   py::implicitly_convertible<VariableBlock<VariableMatrix>, VariableMatrix>();
 }
 
