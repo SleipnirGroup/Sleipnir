e6d4f182606d379b678c5484511034a6bf645ce0
Tyler Veness <calcmogul@gmail.com>
2024-01-21 19:49:16 -0800

    Add missing constraint operators to Python

--- jormungandr/test/optimization/constraints_test.py
+++ jormungandr/test/optimization/constraints_test.py
@@ -1,25 +1,92 @@
-from jormungandr.autodiff import Variable
+from jormungandr.autodiff import Variable, VariableMatrix
 
 
 def test_equality_constraint_boolean_comparisons():
-    assert Variable(1.0) == Variable(1.0)
-    assert not (Variable(1.0) == Variable(2.0))
+    args = [(1.0, 1.0), (1.0, 2.0), (2.0, 1.0)]
+
+    # double-Variable
+    for lhs, rhs in args:
+        assert bool(float(lhs) == Variable(rhs)) == (lhs == rhs)
+
+    # double-VariableMatrix
+    for lhs, rhs in args:
+        assert bool(float(lhs) == VariableMatrix([[rhs]])) == (lhs == rhs)
+
+    # Variable-double
+    for lhs, rhs in args:
+        assert bool(Variable(lhs) == float(rhs)) == (lhs == rhs)
+
+    # Variable-Variable
+    for lhs, rhs in args:
+        assert bool(Variable(lhs) == Variable(rhs)) == (lhs == rhs)
+
+    # Variable-VariableMatrix
+    for lhs, rhs in args:
+        assert bool(Variable(lhs) == VariableMatrix([[rhs]])) == (lhs == rhs)
+
+    # VariableMatrix-double
+    for lhs, rhs in args:
+        assert bool(VariableMatrix([[lhs]]) == float(rhs)) == (lhs == rhs)
+
+    # VariableMatrix-Variable
+    for lhs, rhs in args:
+        assert bool(VariableMatrix([[lhs]]) == Variable(rhs)) == (lhs == rhs)
+
+    # VariableMatrix-VariableMatrix
+    for lhs, rhs in args:
+        assert bool(VariableMatrix([[lhs]]) == VariableMatrix([[rhs]])) == (lhs == rhs)
 
 
+# For the purposes of optimization, a < constraint is treated the same as a <=
+# constraint
 def test_inequality_constraint_boolean_comparisons():
-    # These are all true because for the purposes of optimization, a <
-    # constraint is treated the same as a <= constraint
-    assert Variable(1.0) < Variable(1.0)
-    assert Variable(1.0) <= Variable(1.0)
-    assert Variable(1.0) > Variable(1.0)
-    assert Variable(1.0) >= Variable(1.0)
+    args = [(1.0, 1.0), (1.0, 2.0), (2.0, 1.0)]
 
-    assert Variable(1.0) < Variable(2.0)
-    assert Variable(1.0) <= Variable(2.0)
-    assert not (Variable(1.0) > Variable(2.0))
-    assert not (Variable(1.0) >= Variable(2.0))
+    # double-Variable
+    for lhs, rhs in args:
+        assert bool(float(lhs) < Variable(rhs)) == (lhs <= rhs)
+        assert bool(float(lhs) <= Variable(rhs)) == (lhs <= rhs)
+        assert bool(float(lhs) > Variable(rhs)) == (lhs >= rhs)
+        assert bool(float(lhs) >= Variable(rhs)) == (lhs >= rhs)
 
-    assert not (Variable(2.0) < Variable(1.0))
-    assert not (Variable(2.0) <= Variable(1.0))
-    assert Variable(2.0) > Variable(1.0)
-    assert Variable(2.0) >= Variable(1.0)
+    # double-VariableMatrix
+    for lhs, rhs in args:
+        assert bool(float(lhs) < VariableMatrix([[rhs]])) == (lhs <= rhs)
+        assert bool(float(lhs) <= VariableMatrix([[rhs]])) == (lhs <= rhs)
+        assert bool(float(lhs) > VariableMatrix([[rhs]])) == (lhs >= rhs)
+        assert bool(float(lhs) >= VariableMatrix([[rhs]])) == (lhs >= rhs)
+
+    # Variable-Variable
+    for lhs, rhs in args:
+        assert bool(Variable(lhs) < Variable(rhs)) == (lhs <= rhs)
+        assert bool(Variable(lhs) <= Variable(rhs)) == (lhs <= rhs)
+        assert bool(Variable(lhs) > Variable(rhs)) == (lhs >= rhs)
+        assert bool(Variable(lhs) >= Variable(rhs)) == (lhs >= rhs)
+
+    # Variable-VariableMatrix
+    for lhs, rhs in args:
+        assert bool(Variable(lhs) < VariableMatrix([[rhs]])) == (lhs <= rhs)
+        assert bool(Variable(lhs) <= VariableMatrix([[rhs]])) == (lhs <= rhs)
+        assert bool(Variable(lhs) > VariableMatrix([[rhs]])) == (lhs >= rhs)
+        assert bool(Variable(lhs) >= VariableMatrix([[rhs]])) == (lhs >= rhs)
+
+    # VariableMatrix-double
+    for lhs, rhs in args:
+        assert bool(VariableMatrix([[lhs]]) < float(rhs)) == (lhs <= rhs)
+        assert bool(VariableMatrix([[lhs]]) <= float(rhs)) == (lhs <= rhs)
+        assert bool(VariableMatrix([[lhs]]) > float(rhs)) == (lhs >= rhs)
+        assert bool(VariableMatrix([[lhs]]) >= float(rhs)) == (lhs >= rhs)
+
+    # VariableMatrix-Variable
+    for lhs, rhs in args:
+        assert bool(VariableMatrix([[lhs]]) < Variable(rhs)) == (lhs <= rhs)
+        assert bool(VariableMatrix([[lhs]]) <= Variable(rhs)) == (lhs <= rhs)
+        assert bool(VariableMatrix([[lhs]]) > Variable(rhs)) == (lhs >= rhs)
+        assert bool(VariableMatrix([[lhs]]) >= Variable(rhs)) == (lhs >= rhs)
+
+    # VariableMatrix-VariableMatrix
+    for lhs, rhs in args:
+        assert bool(VariableMatrix([[lhs]]) < VariableMatrix([[rhs]])) == (lhs <= rhs)
+        assert bool(VariableMatrix([[lhs]]) <= VariableMatrix([[rhs]])) == (lhs <= rhs)
+        assert bool(VariableMatrix([[lhs]]) > VariableMatrix([[rhs]])) == (lhs >= rhs)
+        assert bool(VariableMatrix([[lhs]]) >= VariableMatrix([[rhs]])) == (lhs >= rhs)
--- test/src/optimization/ConstraintsTest.cpp
+++ test/src/optimization/ConstraintsTest.cpp
@@ -1,29 +1,131 @@
 // Copyright (c) Sleipnir contributors
 
+#include <array>
+#include <tuple>
+
 #include <gtest/gtest.h>
 #include <sleipnir/autodiff/Variable.hpp>
+#include <sleipnir/autodiff/VariableMatrix.hpp>
 #include <sleipnir/optimization/Constraints.hpp>
 
 TEST(ConstraintsTest, EqualityConstraintBooleanComparisons) {
-  EXPECT_TRUE(sleipnir::Variable{1.0} == sleipnir::Variable{1.0});
-  EXPECT_FALSE(sleipnir::Variable{1.0} == sleipnir::Variable{2.0});
+  using sleipnir::Variable;
+  using sleipnir::VariableMatrix;
+
+  constexpr std::array<std::tuple<double, double>, 3> args{
+      std::tuple{1.0, 1.0}, {1.0, 2.0}, {2.0, 1.0}};
+
+  // double-Variable
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(double{lhs} == Variable{rhs}, lhs == rhs);
+  }
+
+  // double-VariableMatrix
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(double{lhs} == VariableMatrix{{rhs}}, lhs == rhs);
+  }
+
+  // Variable-double
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(Variable{lhs} == double{rhs}, lhs == rhs);
+  }
+
+  // Variable-Variable
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(Variable{lhs} == Variable{rhs}, lhs == rhs);
+  }
+
+  // Variable-VariableMatrix
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(Variable{lhs} == VariableMatrix{{rhs}}, lhs == rhs);
+  }
+
+  // VariableMatrix-double
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(VariableMatrix{{lhs}} == double{rhs}, lhs == rhs);
+  }
+
+  // VariableMatrix-Variable
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(VariableMatrix{{lhs}} == Variable{rhs}, lhs == rhs);
+  }
+
+  // VariableMatrix-VariableMatrix
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(VariableMatrix{{lhs}} == VariableMatrix{{rhs}}, lhs == rhs);
+  }
 }
 
+// For the purposes of optimization, a < constraint is treated the same as a <=
+// constraint
 TEST(ConstraintsTest, InequalityConstraintBooleanComparisons) {
-  // These are all true because for the purposes of optimization, a < constraint
-  // is treated the same as a <= constraint
-  EXPECT_TRUE(sleipnir::Variable{1.0} < sleipnir::Variable{1.0});
-  EXPECT_TRUE(sleipnir::Variable{1.0} <= sleipnir::Variable{1.0});
-  EXPECT_TRUE(sleipnir::Variable{1.0} > sleipnir::Variable{1.0});
-  EXPECT_TRUE(sleipnir::Variable{1.0} >= sleipnir::Variable{1.0});
+  using sleipnir::Variable;
+  using sleipnir::VariableMatrix;
 
-  EXPECT_TRUE(sleipnir::Variable{1.0} < sleipnir::Variable{2.0});
-  EXPECT_TRUE(sleipnir::Variable{1.0} <= sleipnir::Variable{2.0});
-  EXPECT_FALSE(sleipnir::Variable{1.0} > sleipnir::Variable{2.0});
-  EXPECT_FALSE(sleipnir::Variable{1.0} >= sleipnir::Variable{2.0});
+  constexpr std::array<std::tuple<double, double>, 3> args{
+      std::tuple{1.0, 1.0}, {1.0, 2.0}, {2.0, 1.0}};
 
-  EXPECT_FALSE(sleipnir::Variable{2.0} < sleipnir::Variable{1.0});
-  EXPECT_FALSE(sleipnir::Variable{2.0} <= sleipnir::Variable{1.0});
-  EXPECT_TRUE(sleipnir::Variable{2.0} > sleipnir::Variable{1.0});
-  EXPECT_TRUE(sleipnir::Variable{2.0} >= sleipnir::Variable{1.0});
+  // double-Variable
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(double{lhs} < Variable{rhs}, lhs <= rhs);
+    EXPECT_EQ(double{lhs} <= Variable{rhs}, lhs <= rhs);
+    EXPECT_EQ(double{lhs} > Variable{rhs}, lhs >= rhs);
+    EXPECT_EQ(double{lhs} >= Variable{rhs}, lhs >= rhs);
+  }
+
+  // double-VariableMatrix
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(double{lhs} < VariableMatrix{{rhs}}, lhs <= rhs);
+    EXPECT_EQ(double{lhs} <= VariableMatrix{{rhs}}, lhs <= rhs);
+    EXPECT_EQ(double{lhs} > VariableMatrix{{rhs}}, lhs >= rhs);
+    EXPECT_EQ(double{lhs} >= VariableMatrix{{rhs}}, lhs >= rhs);
+  }
+
+  // Variable-double
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(Variable{lhs} < double{rhs}, lhs <= rhs);
+    EXPECT_EQ(Variable{lhs} <= double{rhs}, lhs <= rhs);
+    EXPECT_EQ(Variable{lhs} > double{rhs}, lhs >= rhs);
+    EXPECT_EQ(Variable{lhs} >= double{rhs}, lhs >= rhs);
+  }
+
+  // Variable-Variable
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(Variable{lhs} < Variable{rhs}, lhs <= rhs);
+    EXPECT_EQ(Variable{lhs} <= Variable{rhs}, lhs <= rhs);
+    EXPECT_EQ(Variable{lhs} > Variable{rhs}, lhs >= rhs);
+    EXPECT_EQ(Variable{lhs} >= Variable{rhs}, lhs >= rhs);
+  }
+
+  // Variable-VariableMatrix
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(Variable{lhs} < VariableMatrix{{rhs}}, lhs <= rhs);
+    EXPECT_EQ(Variable{lhs} <= VariableMatrix{{rhs}}, lhs <= rhs);
+    EXPECT_EQ(Variable{lhs} > VariableMatrix{{rhs}}, lhs >= rhs);
+    EXPECT_EQ(Variable{lhs} >= VariableMatrix{{rhs}}, lhs >= rhs);
+  }
+
+  // VariableMatrix-double
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(VariableMatrix{{lhs}} < double{rhs}, lhs <= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} <= double{rhs}, lhs <= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} > double{rhs}, lhs >= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} >= double{rhs}, lhs >= rhs);
+  }
+
+  // VariableMatrix-Variable
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(VariableMatrix{{lhs}} < Variable{rhs}, lhs <= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} <= Variable{rhs}, lhs <= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} > Variable{rhs}, lhs >= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} >= Variable{rhs}, lhs >= rhs);
+  }
+
+  // VariableMatrix-VariableMatrix
+  for (const auto& [lhs, rhs] : args) {
+    EXPECT_EQ(VariableMatrix{{lhs}} < VariableMatrix{{rhs}}, lhs <= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} <= VariableMatrix{{rhs}}, lhs <= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} > VariableMatrix{{rhs}}, lhs >= rhs);
+    EXPECT_EQ(VariableMatrix{{lhs}} >= VariableMatrix{{rhs}}, lhs >= rhs);
+  }
 }
