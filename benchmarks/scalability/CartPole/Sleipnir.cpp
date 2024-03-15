79b95a6caa4d097d375d01dd122febb3293cedbc
Tyler Veness <calcmogul@gmail.com>
2024-03-14 23:47:49 -0700

    Refactor benchmark RK4 into separate file (#442)

--- benchmarks/scalability/CartPole/Sleipnir.cpp
+++ benchmarks/scalability/CartPole/Sleipnir.cpp
@@ -7,25 +7,7 @@
 
 #include <Eigen/Core>
 
-/**
- * Performs 4th order Runge-Kutta integration of dx/dt = f(x, u) for dt.
- *
- * @param f  The function to integrate. It must take two arguments x and u.
- * @param x  The initial value of x.
- * @param u  The value u held constant over the integration period.
- * @param dt The time over which to integrate.
- */
-template <typename F, typename T, typename U>
-T RK4(F&& f, T x, U u, std::chrono::duration<double> dt) {
-  const auto h = dt.count();
-
-  T k1 = f(x, u);
-  T k2 = f(x + h * 0.5 * k1, u);
-  T k3 = f(x + h * 0.5 * k2, u);
-  T k4 = f(x + h * k3, u);
-
-  return x + h / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
-}
+#include "RK4.hpp"
 
 sleipnir::VariableMatrix CartPoleDynamics(const sleipnir::VariableMatrix& x,
                                           const sleipnir::VariableMatrix& u) {
