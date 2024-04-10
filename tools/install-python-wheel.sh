80f69af1b00cba7408344588ec050d85e2955641
Tyler Veness <calcmogul@gmail.com>
2024-04-09 18:56:07 -0700

    Delete old .whl files before building another (#497)

--- tools/install-python-wheel.sh
+++ tools/install-python-wheel.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Builds and installs Python wheel for local testing
+rm -f dist/*.whl
 python -m build --wheel && pip install --user --break-system-packages --force-reinstall dist/*.whl
