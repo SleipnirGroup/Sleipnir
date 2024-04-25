1441369554d1b6a7b1fc9e567ad67d93dd028894
Tyler Veness <calcmogul@gmail.com>
2024-04-24 20:22:32 -0700

    Templatize formatters
    This is required for compatibility with libc++'s std::format.

--- include/sleipnir/util/Formatters.hpp
+++ include/sleipnir/util/Formatters.hpp
@@ -27,11 +27,13 @@ template <typename Derived, typename CharT>
   requires std::derived_from<Derived, Eigen::DenseBase<Derived>> ||
            std::derived_from<Derived, Eigen::SparseCompressedBase<Derived>>
 struct fmt::formatter<Derived, CharT> {
-  constexpr auto parse(fmt::format_parse_context& ctx) {
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext& ctx) {
     return m_underlying.parse(ctx);
   }
 
-  auto format(const Derived& mat, fmt::format_context& ctx) const {
+  template <typename FmtContext>
+  auto format(const Derived& mat, FmtContext& ctx) const {
     auto out = ctx.out();
 
     for (int row = 0; row < mat.rows(); ++row) {
@@ -57,12 +59,13 @@ struct fmt::formatter<Derived, CharT> {
  */
 template <>
 struct fmt::formatter<sleipnir::Variable> {
-  constexpr auto parse(fmt::format_parse_context& ctx) {
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext& ctx) {
     return m_underlying.parse(ctx);
   }
 
-  auto format(const sleipnir::Variable& variable,
-              fmt::format_context& ctx) const {
+  template <typename FmtContext>
+  auto format(const sleipnir::Variable& variable, FmtContext& ctx) const {
     return m_underlying.format(variable.Value(), ctx);
   }
 
@@ -77,11 +80,13 @@ template <typename T>
   requires std::same_as<T, sleipnir::VariableBlock<sleipnir::VariableMatrix>> ||
            std::same_as<T, sleipnir::VariableMatrix>
 struct fmt::formatter<T> {
-  constexpr auto parse(fmt::format_parse_context& ctx) {
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext& ctx) {
     return m_underlying.parse(ctx);
   }
 
-  auto format(const T& mat, fmt::format_context& ctx) const {
+  template <typename FmtContext>
+  auto format(const T& mat, FmtContext& ctx) const {
     auto out = ctx.out();
 
     for (int row = 0; row < mat.Rows(); ++row) {
