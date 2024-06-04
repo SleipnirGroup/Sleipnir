7180bb053412a934ff16d84dc1646bc095d37587
Tyler Veness <calcmogul@gmail.com>
2024-06-03 20:50:57 -0700

    Update Python solve() arguments and fix docs

--- jormungandr/cpp/optimization/BindOptimizationProblem.cpp
+++ jormungandr/cpp/optimization/BindOptimizationProblem.cpp
@@ -69,15 +69,28 @@ void BindOptimizationProblem(py::class_<OptimizationProblem>& cls) {
       "solve",
       [](OptimizationProblem& self, const py::kwargs& kwargs) {
         SolverConfig config;
+
         for (auto& [key, value] : kwargs) {
+          // XXX: The keyword arguments are manually copied from the struct
+          // members in include/sleipnir/optimization/SolverConfig.hpp.
+          //
+          // C++'s OptimizationProblem::Solve() takes a SolverConfig object
+          // instead of keyword arguments, so there's no compile-time checking
+          // that the arguments match.
           auto key_str = key.cast<std::string>();
           if (key_str == "tolerance") {
             config.tolerance = value.cast<double>();
           } else if (key_str == "max_iterations") {
             config.maxIterations = value.cast<int>();
+          } else if (key_str == "acceptable_tolerance") {
+            config.acceptableTolerance = value.cast<double>();
+          } else if (key_str == "max_acceptable_iterations") {
+            config.maxAcceptableIterations = value.cast<int>();
           } else if (key_str == "timeout") {
             config.timeout =
                 std::chrono::duration<double>{value.cast<double>()};
+          } else if (key_str == "feasible_ipm") {
+            config.feasibleIPM = value.cast<bool>();
           } else if (key_str == "diagnostics") {
             config.diagnostics = value.cast<bool>();
           } else if (key_str == "spy") {
@@ -90,7 +103,57 @@ void BindOptimizationProblem(py::class_<OptimizationProblem>& cls) {
 
         return self.Solve(config);
       },
-      DOC(sleipnir, OptimizationProblem, Solve));
+      // XXX: The keyword argument docs are manually copied from the struct
+      // member docs in include/sleipnir/optimization/SolverConfig.hpp.
+      //
+      // C++'s OptimizationProblem::Solve() takes a SolverConfig object instead
+      // of keyword arguments, so pybind11-mkdoc generates the wrong docs.
+      R"doc(Solve the optimization problem. The solution will be stored in the
+original variables used to construct the problem.
+
+Parameter ``tolerance``:
+    The solver will stop once the error is below this tolerance.
+    (default: 1e-8)
+
+Parameter ``max_iterations``:
+    The maximum number of solver iterations before returning a solution.
+    (default: 5000)
+
+Parameter ``acceptable_tolerance``:
+    The solver will stop once the error is below this tolerance for
+    `acceptable_iterations` iterations. This is useful in cases where the
+    solver might not be able to achieve the desired level of accuracy due to
+    floating-point round-off.
+    (default: 1e-6)
+
+Parameter ``max_acceptable_iterations``:
+    The solver will stop once the error is below `acceptable_tolerance` for
+    this many iterations.
+    (default: 15)
+
+Parameter ``timeout``:
+    The maximum elapsed wall clock time before returning a solution.
+    (default: infinity)
+
+Parameter ``feasible_ipm``:
+    Enables the feasible interior-point method. When the inequality
+    constraints are all feasible, step sizes are reduced when necessary to
+    prevent them becoming infeasible again. This is useful when parts of the
+    problem are ill-conditioned in infeasible regions (e.g., square root of a
+    negative value). This can slow or prevent progress toward a solution
+    though, so only enable it if necessary.
+    (default: False)
+
+Parameter ``diagnostics``:
+    Enables diagnostic prints.
+    (default: False)
+
+Parameter ``spy``:
+    Enables writing sparsity patterns of H, Aₑ, and Aᵢ to files named H.spy,
+    A_e.spy, and A_i.spy respectively during solve.
+
+    Use tools/spy.py to plot them.
+    (default: False))doc");
   cls.def(
       "callback",
       [](OptimizationProblem& self,
