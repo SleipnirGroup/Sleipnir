928354b1ebb2a79a06771a403908f64970510a14
Tyler Veness <calcmogul@gmail.com>
2025-01-15 21:54:50 -0800

    Add cooldown periods to benchmark script

--- tools/generate-scalability-results.sh
+++ tools/generate-scalability-results.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 ./build/FlywheelScalabilityBenchmark --casadi
+
+echo -n "15 second cooldown..."
+sleep 15
+echo " done."
+
 ./build/FlywheelScalabilityBenchmark --sleipnir
+
 ./tools/plot_scalability_results.py \
   --filenames \
     flywheel-scalability-results-casadi.csv \
@@ -11,8 +18,18 @@
   --title Flywheel \
   --noninteractive
 
+echo -n "15 second cooldown..."
+sleep 15
+echo " done."
+
 ./build/CartPoleScalabilityBenchmark --casadi
+
+echo -n "30 second cooldown..."
+sleep 30
+echo " done."
+
 ./build/CartPoleScalabilityBenchmark --sleipnir
+
 ./tools/plot_scalability_results.py \
   --filenames \
     cart-pole-scalability-results-casadi.csv \
