adc8312b8b480f254cbd77cd8fe0ed3649c6033a
Tyler Veness <calcmogul@gmail.com>
2025-01-18 13:10:57 -0800

    Add perf benchmark

---
+++ tools/perf-benchmark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [[ $# -ne 1 ]] || [[ "$1" != "CartPole" && "$1" != "Flywheel" ]]; then
+  echo "usage: ./tools/perf-benchmark.sh {CartPole,Flywheel}"
+  exit 1
+fi
+
+cmake -B build-perf -S . -DCMAKE_BUILD_TYPE=Perf -DBUILD_BENCHMARKING=ON
+cmake --build build-perf --target $1PerfBenchmark
+./tools/perf-record.sh ./build-perf/$1PerfBenchmark
+./tools/perf-report.sh
