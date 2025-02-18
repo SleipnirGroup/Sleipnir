c5e7fe11cefa30dc7dc175d5ef47284d64370ee7
Tyler Veness <calcmogul@gmail.com>
2025-02-17 21:50:06 -0800

    Move std::decay_t call to concept definitions

--- include/sleipnir/util/Concepts.hpp
+++ include/sleipnir/util/Concepts.hpp
@@ -3,13 +3,14 @@
 #pragma once
 
 #include <concepts>
+#include <type_traits>
 
 #include <Eigen/Core>
 
 namespace sleipnir {
 
 template <typename T>
-concept ScalarLike = requires(T t) {
+concept ScalarLike = requires(std::decay_t<T> t) {
   t + 1.0;
   t = 1.0;
 };
@@ -22,7 +23,8 @@ concept SleipnirMatrixLike = requires(T t, int rows, int cols) {
 };
 
 template <typename T>
-concept EigenMatrixLike = std::derived_from<T, Eigen::MatrixBase<T>>;
+concept EigenMatrixLike =
+    std::derived_from<std::decay_t<T>, Eigen::MatrixBase<std::decay_t<T>>>;
 
 template <typename T>
 concept MatrixLike = SleipnirMatrixLike<T> || EigenMatrixLike<T>;
