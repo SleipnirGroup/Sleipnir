e46edb5957e1757749f49171710f4102fb1920f6
Tyler Veness <calcmogul@gmail.com>
2025-02-22 00:50:56 -0800

    Speed up Σ and S⁻¹e calculations (#783)

--- jormungandr/test/optimization/optimization_problem_arm_on_elevator_test.py
+++ jormungandr/test/optimization/optimization_problem_arm_on_elevator_test.py
@@ -1,7 +1,6 @@
 """This problem tests the case where regularization fails"""
 
 import math
-import platform
 
 import numpy as np
 
@@ -99,9 +98,4 @@ def test_optimization_problem_arm_on_elevator():
     assert status.cost_function_type == ExpressionType.QUADRATIC
     assert status.equality_constraint_type == ExpressionType.LINEAR
     assert status.inequality_constraint_type == ExpressionType.LINEAR
-
-    if platform.system() == "Linux" and platform.machine() == "aarch64":
-        # FIXME: Fails on Linux aarch64 with "factorization failed"
-        assert status.exit_condition == SolverExitCondition.FACTORIZATION_FAILED
-    else:
-        assert status.exit_condition == SolverExitCondition.SUCCESS
+    assert status.exit_condition == SolverExitCondition.SUCCESS
