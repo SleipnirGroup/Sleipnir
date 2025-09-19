57eb59d41766c016394c144197ea5da13846416c
Tyler Veness <calcmogul@gmail.com>
2025-09-18 23:47:54 -0700

    Invalidate Variable graph on expression change (#914)

--- include/sleipnir/autodiff/variable.hpp
+++ include/sleipnir/autodiff/variable.hpp
@@ -88,6 +88,7 @@ class SLEIPNIR_DLLEXPORT Variable {
    */
   Variable& operator=(double value) {
     expr = detail::make_expression_ptr<detail::ConstExpression>(value);
+    m_graph_initialized = false;
 
     return *this;
   }
@@ -98,22 +99,18 @@ class SLEIPNIR_DLLEXPORT Variable {
    * @param value The value of the Variable.
    */
   void set_value(double value) {
-    if (expr->is_constant(0.0)) {
-      expr = detail::make_expression_ptr<detail::ConstExpression>(value);
-    } else {
 #ifndef SLEIPNIR_DISABLE_DIAGNOSTICS
-      // We only need to check the first argument since unary and binary
-      // operators both use it
-      if (expr->args[0] != nullptr) {
-        auto location = std::source_location::current();
-        slp::println(
-            stderr,
-            "WARNING: {}:{}: {}: Modified the value of a dependent variable",
-            location.file_name(), location.line(), location.function_name());
-      }
-#endif
-      expr->val = value;
+    // We only need to check the first argument since unary and binary operators
+    // both use it
+    if (expr->args[0] != nullptr) {
+      auto location = std::source_location::current();
+      slp::println(
+          stderr,
+          "WARNING: {}:{}: {}: Modified the value of a dependent variable",
+          location.file_name(), location.line(), location.function_name());
     }
+#endif
+    expr->val = value;
   }
 
   /**
