2eacdde898f7765fcaafb7e3ae936999ff6f5056
Tyler Veness <calcmogul@gmail.com>
2025-10-14 14:25:17 -0700

    Simplify CMake settings (#954)

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,35 +1,16 @@
 macro(compiler_flags target)
     if(NOT MSVC)
-        target_compile_options(
-            ${target}
-            PRIVATE -Wall -pedantic -Wextra -Werror -Wno-psabi
-        )
+        target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic)
     else()
         # Suppress the following warnings:
         #   * C4244: lossy conversion
         #   * C4251: missing dllexport/dllimport attribute on data member
-        target_compile_options(${target} PRIVATE /wd4244 /wd4251 /WX)
+        target_compile_options(${target} PRIVATE /wd4244 /wd4251)
     endif()
+    set_property(TARGET ${target} PROPERTY COMPILE_WARNING_AS_ERROR ON)
 
-    # Disable warning false positives in Eigen
-    if(
-        ${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU"
-        AND ${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER_EQUAL "12"
-    )
-        target_compile_options(
-            ${target}
-            PRIVATE -Wno-array-bounds -Wno-stringop-overflow
-        )
-    endif()
-
+    target_compile_features(${target} PUBLIC cxx_std_23)
     if(MSVC)
-        target_compile_options(
-            ${target}
-            PUBLIC /std:c++23preview /MP /utf-8 /bigobj
-        )
-    endif()
-
-    if(NOT MSVC)
-        target_compile_features(${target} PUBLIC cxx_std_23)
+        target_compile_options(${target} PUBLIC /MP /Zf /utf-8 /bigobj)
     endif()
 endmacro()
