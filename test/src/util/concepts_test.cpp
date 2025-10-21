89bacab4feb5bf34a52c299fe980ada900cda75d
Tyler Veness <calcmogul@gmail.com>
2025-10-21 11:31:16 -0700

    Add more scalar types to concepts test (#963)

--- test/src/util/concepts_test.cpp
+++ test/src/util/concepts_test.cpp
@@ -7,7 +7,6 @@
 
 using EigBlk = Eigen::Block<Eigen::MatrixXd>;
 using EigMat = Eigen::MatrixXd;
-using EigVar = double;
 
 using SlpBlk = slp::VariableBlock<slp::VariableMatrix>;
 using SlpMat = slp::VariableMatrix;
@@ -16,7 +15,8 @@ using SlpVar = slp::Variable;
 // EigenMatrixLike
 static_assert(slp::EigenMatrixLike<EigBlk>);
 static_assert(slp::EigenMatrixLike<EigMat>);
-static_assert(!slp::EigenMatrixLike<EigVar>);
+static_assert(!slp::EigenMatrixLike<double>);
+static_assert(!slp::EigenMatrixLike<int>);
 static_assert(!slp::EigenMatrixLike<SlpBlk>);
 static_assert(!slp::EigenMatrixLike<SlpMat>);
 static_assert(!slp::EigenMatrixLike<SlpVar>);
@@ -24,7 +24,8 @@ static_assert(!slp::EigenMatrixLike<SlpVar>);
 // SleipnirMatrixLike
 static_assert(!slp::SleipnirMatrixLike<EigBlk>);
 static_assert(!slp::SleipnirMatrixLike<EigMat>);
-static_assert(!slp::SleipnirMatrixLike<EigVar>);
+static_assert(!slp::SleipnirMatrixLike<double>);
+static_assert(!slp::SleipnirMatrixLike<int>);
 static_assert(slp::SleipnirMatrixLike<SlpBlk>);
 static_assert(slp::SleipnirMatrixLike<SlpMat>);
 static_assert(!slp::SleipnirMatrixLike<SlpVar>);
@@ -32,7 +33,8 @@ static_assert(!slp::SleipnirMatrixLike<SlpVar>);
 // MatrixLike
 static_assert(slp::MatrixLike<EigBlk>);
 static_assert(slp::MatrixLike<EigMat>);
-static_assert(!slp::MatrixLike<EigVar>);
+static_assert(!slp::MatrixLike<double>);
+static_assert(!slp::MatrixLike<int>);
 static_assert(slp::MatrixLike<SlpBlk>);
 static_assert(slp::MatrixLike<SlpMat>);
 static_assert(!slp::MatrixLike<SlpVar>);
@@ -40,7 +42,8 @@ static_assert(!slp::MatrixLike<SlpVar>);
 // SleipnirScalarLike
 static_assert(!slp::SleipnirScalarLike<EigBlk>);
 static_assert(!slp::SleipnirScalarLike<EigMat>);
-static_assert(!slp::SleipnirScalarLike<EigVar>);
+static_assert(!slp::SleipnirScalarLike<double>);
+static_assert(!slp::SleipnirScalarLike<int>);
 static_assert(!slp::SleipnirScalarLike<SlpBlk>);
 static_assert(!slp::SleipnirScalarLike<SlpMat>);
 static_assert(slp::SleipnirScalarLike<SlpVar>);
@@ -48,7 +51,8 @@ static_assert(slp::SleipnirScalarLike<SlpVar>);
 // ScalarLike
 static_assert(!slp::ScalarLike<EigBlk>);
 static_assert(!slp::ScalarLike<EigMat>);
-static_assert(slp::ScalarLike<EigVar>);
+static_assert(slp::ScalarLike<double>);
+static_assert(slp::ScalarLike<int>);
 static_assert(!slp::ScalarLike<SlpBlk>);
 static_assert(!slp::ScalarLike<SlpMat>);
 static_assert(slp::ScalarLike<SlpVar>);
@@ -56,7 +60,8 @@ static_assert(slp::ScalarLike<SlpVar>);
 // SleipnirType
 static_assert(!slp::SleipnirType<EigBlk>);
 static_assert(!slp::SleipnirType<EigMat>);
-static_assert(!slp::SleipnirType<EigVar>);
+static_assert(!slp::SleipnirType<double>);
+static_assert(!slp::SleipnirType<int>);
 static_assert(slp::SleipnirType<SlpBlk>);
 static_assert(slp::SleipnirType<SlpMat>);
 static_assert(slp::SleipnirType<SlpVar>);
