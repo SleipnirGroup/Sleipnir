cd27694f9c2dc22eadc46f1c429e81937b8cfbb5
Tyler Veness <calcmogul@gmail.com>
2025-12-29 11:43:35 -0800

    Fix grouping symbol typo in comment (#1089)

--- examples/frc_2024_shooter/main.py
+++ examples/frc_2024_shooter/main.py
@@ -130,7 +130,7 @@ def main():
 
     # Require initial velocity is below max
     #
-    #   √{v_x² + v_y² + v_z²) ≤ vₘₐₓ
+    #   √(v_x² + v_y² + v_z²) ≤ vₘₐₓ
     #   v_x² + v_y² + v_z² ≤ vₘₐₓ²
     problem.subject_to(
         (x[3] - robot_wrt_field[3, 0]) ** 2
