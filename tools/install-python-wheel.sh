0dd732e82c27be159d4b6d34402a9b8ab21487f5
Tyler Veness <calcmogul@gmail.com>
2025-12-30 17:00:52 -0800

    Add script for running pytest on local files (#1090)

--- tools/install-python-wheel.sh
+++ tools/install-python-wheel.sh
@@ -4,6 +4,5 @@ set -e
 rm -rf .py-build-cmake_cache/
 rm -f dist/*.whl
 
-# Builds and installs Python wheel for local testing
 python -m build --wheel
 pip install --user --break-system-packages --force-reinstall dist/*.whl
