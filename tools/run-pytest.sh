f482c9d5161ad8e8268347c1a223165df4123ad3
Tyler Veness <calcmogul@gmail.com>
2026-03-05 23:33:33 -0800

    Add argument passthrough to run-pytest.sh (#1133)

--- tools/run-pytest.sh
+++ tools/run-pytest.sh
@@ -3,6 +3,6 @@ set -e
 
 cmake --build build -t _sleipnir
 cp build/_sleipnir.abi3.so python/src/sleipnir
-PYTHONPATH=python/src pytest
+PYTHONPATH=python/src pytest "$@"
 
 rm python/src/sleipnir/_sleipnir.abi3.so
