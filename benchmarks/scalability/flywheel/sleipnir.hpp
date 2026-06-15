f0db3459423dbfcc447fc65e4cf9f78cdd5e909b
Tyler Veness <calcmogul@gmail.com>
2026-06-14 23:27:47 -0700

    Remove unused function prototype from benchmark header (#1235)

--- benchmarks/scalability/flywheel/sleipnir.hpp
+++ benchmarks/scalability/flywheel/sleipnir.hpp
@@ -11,10 +11,3 @@
 /// @param dt Timestep duration.
 /// @param N Number of samples in the problem.
 slp::Problem<double> flywheel_sleipnir(std::chrono::duration<double> dt, int N);
-
-/// Creates a cart-pole nonlinear optimization problem with Sleipnir.
-///
-/// @param dt Timestep duration.
-/// @param N Number of samples in the problem.
-slp::Problem<double> cart_pole_sleipnir(std::chrono::duration<double> dt,
-                                        int N);
