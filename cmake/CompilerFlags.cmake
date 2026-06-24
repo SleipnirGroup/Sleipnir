fc3863cc7c19a63ed23dbe34418a9cbcd5a27e13
shueja-personal <j.a.shue@outlook.com>
2026-06-23 19:33:11 -0700

    Fix compiler flags to support Clang-cl missing /Zf

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -11,6 +11,12 @@ macro(compiler_flags target)
 
     target_compile_features(${target} PUBLIC cxx_std_23)
     if(MSVC)
-        target_compile_options(${target} PUBLIC /MP /Zf /utf-8 /bigobj)
+        target_compile_options(${target} PUBLIC /MP /utf-8 /bigobj)
+        # /Zf is an MSVC argument not currently supported by Clang-cl, see https://github.com/llvm/llvm-project/issues/51578
+        if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang" AND CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+        else()
+            target_compile_options(${target} PUBLIC /Zf)
+        endif()
     endif()
+    
 endmacro()
